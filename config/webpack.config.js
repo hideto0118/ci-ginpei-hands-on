4343eb496bb74d860215dfd883fb45619a64f365
Hideto Yasunori <hideto0118yasunori@gmail.com>
2019-03-16 17:13:56 -0700

    Modify publicpath

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const InterpolateHtmlPlugin = require('interpolate-html-plugin');
 
 const isProduction = process.env.NODE_ENV === 'production';
-const publicPath = isProduction ? '/hannya-roll' : '';
+const publicPath = isProduction ? '/hannya-ci-ginpei-hands-on' : '';
 
 module.exports = {
   mode: isProduction ? 'production' : 'development',
